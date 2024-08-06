68fa1f08c6239cd317226f588a1e288b4f357010
Justin <justinherrera013@yahoo.com>
2024-08-07 02:32:34 +0800

    fixed styles of search input

--- src/app/components/search-input.tsx
+++ src/app/components/search-input.tsx
@@ -4,7 +4,7 @@ export default function SearchInput() {
   return (
     <div className="flex justify-start items-center bg-[#2E333D] rounded-md py-2 px-4 space-x-2 w-full ">
       <Search color="#9CA3AF" size={20} />
-      <input type="text" className="bg-[#2E333D] rounded-md placeholder:text-sm " placeholder="Search" />
+      <input type="text" className="bg-[#2E333D] rounded-md placeholder:text-sm w-full focus:outline-none text-[#9CA3AF]" placeholder="Search" />
     </div>
   )
 }
