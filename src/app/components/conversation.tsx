2624d6bf6b2f85a5e002e3f991695e602d800222
Justin <justinherrera013@yahoo.com>
2024-08-07 02:26:26 +0800

    complete ui of user chat info

--- src/app/components/conversation.tsx
+++ src/app/components/conversation.tsx
@@ -1,11 +1,16 @@
-import { EllipsisVertical } from "lucide-react";
+import { EllipsisVertical, Paperclip } from "lucide-react";
 import Image from "next/image";
 import UserChatInfo from "@/app/components/user-chat-info";
 
 export default function Conversation() {
   return (
-    <div className="p-4 w-[75%]">
+    <div className="p-4 w-[75%] flex flex-col">
       <UserChatInfo />
+      <div className="mt-auto">
+        <Paperclip />
+        <input type="text" className="" placeholder="Your messsage" />
+      </div>
+     
     </div>
   )
 }
--- src/app/components/user-chat-info.tsx
+++ src/app/components/user-chat-info.tsx
@@ -1,13 +1,18 @@
-import { EllipsisVertical } from "lucide-react";
+import { EllipsisVertical, Phone, Video } from "lucide-react";
 import Image from "next/image";
 
 export default function UserChatInfo() {
   return (
-    <div className="flex space-x-2 px-2 py-2 cursor-pointer">
+    <div className="flex space-x-2 px-2 py-2">
       <Image src="/avatar1.jpg" className="rounded-lg w-12 h-12" alt="avatar" width={500} height={500} />
       <div className="flex items-center w-full">
         <span className="text-white text-xl mb-1">Kim Zairra</span>
-        <EllipsisVertical className="ml-auto" color="#2E333D" />
+        <div className="flex space-x-6 ml-auto">
+          <Phone color="#828498" className="cursor-pointer" />
+          <Video color="#828498" className="cursor-pointer" />
+          <EllipsisVertical color="#828498" className="cursor-pointer" />
+        </div>
+
       </div>
     </div>
   )
