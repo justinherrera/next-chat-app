9212c9d1ae74808ad446abb8d4d93b61407f2c8e
Justin <justinherrera013@yahoo.com>
2024-08-07 02:32:08 +0800

    complete ui of chat input with send button

--- src/app/components/conversation.tsx
+++ src/app/components/conversation.tsx
@@ -1,14 +1,15 @@
-import { EllipsisVertical, Paperclip } from "lucide-react";
+import { EllipsisVertical, Mic, Paperclip, SendHorizontal } from "lucide-react";
 import Image from "next/image";
 import UserChatInfo from "@/app/components/user-chat-info";
+import ChatInput from "./chat-input";
 
 export default function Conversation() {
   return (
     <div className="p-4 w-[75%] flex flex-col">
       <UserChatInfo />
-      <div className="mt-auto">
-        <Paperclip />
-        <input type="text" className="" placeholder="Your messsage" />
+      <div className="flex items-center mt-auto px-4">
+        <ChatInput />
+        <SendHorizontal color="#9CA3AF" size={20} className="cursor-pointer" />
       </div>
      
     </div>
--- src/app/components/user-chat.tsx
+++ src/app/components/user-chat.tsx
@@ -7,7 +7,7 @@ export default function UserChat({ user, key }: { user: any, key: number }) {
       <Image src={user.avatar} className="rounded-lg w-16 h-16" alt="avatar" width={500} height={500} />
       <div className="flex flex-col space-y-1 w-full">
         <span className="text-white mb-1">{user.name}</span>
-        <span className="text-sm text-[#a3a3a3]">{user.message}</span>
+        <span className="text-sm text-[#828498]">{user.message}</span>
       </div>
       <div className="w-20 ">
         <span className="text-xs text-[#a3a3a3]">{user.time}</span>
