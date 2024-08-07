6b63dfc98ef577593824c7d0f894b13d83be2e7d
Justin <justinherrera013@yahoo.com>
2024-08-07 20:46:46 +0800

    resized side menu buttons

--- src/app/components/side-menu.tsx
+++ src/app/components/side-menu.tsx
@@ -5,28 +5,28 @@ import { MessageCircle, UserPlus, Users, Archive, Settings } from "lucide-react"
 
 export default function SideMenu() {
   return (
-  <div className="flex flex-col h-full w-[6%] p-4">
+  <div className="flex flex-col h-full w-[5%] p-4">
     <div className="flex flex-col space-y-8">
       <div className="flex flex-col justify-center items-center space-y-2 cursor-pointer bg-[#464646] rounded-lg py-2 px-4">
-        <MessageCircle color="#FFFFFF" size={30} />
+        <MessageCircle color="#FFFFFF" size={24} />
         <span className="text-xs text-white">Chats</span>
       </div>
       <div className="flex flex-col justify-center items-center space-y-2 cursor-pointer">
-        <Users color="#9c9c9c" size={30} />
+        <Users color="#9c9c9c" size={24} />
         <span className="text-xs text-[#9c9c9c]">People</span>
       </div>
       <div className="flex flex-col justify-center items-center space-y-2 cursor-pointer">
-        <UserPlus color="#9c9c9c" size={30} />
+        <UserPlus color="#9c9c9c" size={24} />
         <span className="text-xs text-[#9c9c9c]">Requests</span>
       </div>
       <div className="flex flex-col justify-center items-center space-y-2 cursor-pointer">
-        <Archive color="#9c9c9c" size={30} />
+        <Archive color="#9c9c9c" size={24} />
         <span className="text-xs text-[#9c9c9c]">Archive</span>
       </div>
     </div>
 
     <div className="flex flex-col justify-center items-center space-y-2 cursor-pointer mt-auto">
-      <Settings color="#9c9c9c" size={30} />
+      <Settings color="#9c9c9c" size={24} />
       <span className="text-xs text-[#9c9c9c]">Settings</span>
     </div>
   </div>
