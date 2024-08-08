95a8829b42bea8669f0f6de0a92b20ce16e2cf41
Justin <justinherrera013@yahoo.com>
2024-08-08 14:58:19 +0800

    modified chat-details and some redesigns

--- src/app/components/chat-details.tsx
+++ src/app/components/chat-details.tsx
@@ -1,14 +1,14 @@
 import { Ban, Bell, Search, TriangleAlert, UserPlus, X, File } from "lucide-react";
-
+import Image from "next/image";
 
 export default function ChatDetails() {
   return (
     <div className="flex flex-col w-[19%] p-4">
       <div className="flex justify-between mt-4 w-full">
-        <span className="text-white text-xl mb-1">Chat Details</span>
+        <span className="text-white text-lg mb-1">Chat Details</span>
         <X size={30} color="#FFFFFF" className="cursor-pointer" />
       </div>
-      <div className="flex justify-center space-x-4 mt-12">
+      <div className="flex justify-center space-x-4 mt-10">
         <div className="cursor-pointer bg-[#202329] rounded-lg py-4 px-4">
           <Search size={24} color="#FFFFFF" className="cursor-pointer" />
         </div>
@@ -26,12 +26,20 @@ export default function ChatDetails() {
         </div>
       </div>
       <div className="flex justify-between items-center mt-8 w-full ">
-        <span className="text-white text-xl">Photos and Videos</span>
-        <span className="text-[#a3a3a3] text-xs underline">See all</span>
+        <span className="text-white text-lg">Photos and Videos</span>
+        <span className="text-[#a3a3a3] text-xs underline cursor-pointer">See all</span>
+      </div>
+      <div className="grid gap-y-2 grid-cols-3 mt-4">
+        <Image src="/media1.jpg" className="rounded-lg w-24 h-20" alt="avatar" width={500} height={500} />
+        <Image src="/media1.jpg" className="rounded-lg w-24 h-20" alt="avatar" width={500} height={500} />
+        <Image src="/media1.jpg" className="rounded-lg w-24 h-20" alt="avatar" width={500} height={500} />
+        <Image src="/media1.jpg" className="rounded-lg w-24 h-20" alt="avatar" width={500} height={500} />
+        <Image src="/media1.jpg" className="rounded-lg w-24 h-20" alt="avatar" width={500} height={500} />
+        <span className="text-[#a3a3a3] text-base px-8 py-8">32+</span>
       </div>
       <div className="flex justify-between items-center mt-8 w-full ">
-        <span className="text-white text-xl">Shared Files</span>
-        <span className="text-[#a3a3a3] text-xs underline">See all</span>
+        <span className="text-white text-lg">Shared Files</span>
+        <span className="text-[#a3a3a3] text-xs underline cursor-pointer">See all</span>
       </div>
       <div className="flex flex-col space-y-4 mt-4">
         <div className="flex justify-start items-center space-x-4">
