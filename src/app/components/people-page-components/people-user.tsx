620a5716331c75cbdbee7a43e5b26c8ccee6fe23
Justin <justinherrera013@yahoo.com>
2024-08-08 15:52:36 +0800

    added fake users in people list

--- src/app/components/people-page-components/people-user.tsx
+++ src/app/components/people-page-components/people-user.tsx
@@ -1,17 +1,71 @@
 import Image from "next/image";
 
-
+const users = [
+  {
+    id: 1,
+    name: "Kim Zairra",
+    avatar: "/avatar1.jpg",
+  },
+  {
+    id: 2,
+    name: "John Doe",
+    avatar: "/avatar2.jpg",
+  },
+  {
+    id: 3,
+    name: "Jack Smith",
+    avatar: "/avatar3.jpg",
+  },
+  {
+    id: 4,
+    name: "Emily Davis",
+    avatar: "/avatar5.jpg",
+  },
+  {
+    id: 5,
+    name: "Michael Brown",
+    avatar: "/avatar4.jpg",
+  },
+  {
+    id: 6,
+    name: "Sarah Johnson",
+    avatar: "/avatar6.jpg",
+  },
+  {
+    id: 7,
+    name: "James Wilson",
+    avatar: "/avatar2.jpg",
+  },
+  {
+    name: "Robert Miller",
+    avatar: "/avatar3.jpg",
+  },
+  {
+    id: 9,
+    name: "David Martinez",
+    avatar: "/avatar4.jpg",
+  },
+  {
+    id: 10,
+    name: "Aliah Anderson",
+    avatar: "/avatar1.jpg",
+  },
+];
 export default function PeopleUser() {
   return (
-    <div className="flex space-x-2 px-2 rounded-lg py-2 cursor-pointer hover:bg-[#2E333D]">
-      <Image src="/avatar1.jpg" className="rounded-lg w-16 h-16" alt="avatar" width={500} height={500} />
-      <div className="flex flex-col space-y-1 w-full">
-        <span className="text-white mb-1">Kim Zairra</span>
-        {/* <span className="text-sm text-[#828498]">{user.message}</span> */}
-      </div>
-      {/* <div className="w-20 ">
-        <span className="text-xs text-[#a3a3a3]">{user.time}</span>
-      </div> */}
+    <div className="flex flex-col space-y-2">
+      {
+        users.map((user, index) => {
+          return (<div className="flex space-x-2 px-2 rounded-lg py-2 cursor-pointer hover:bg-[#2E333D]" key={user.id}>
+            <Image src={user.avatar} className="rounded-lg w-12 h-12" alt="avatar" width={500} height={500} />
+            <div className="flex flex-col space-y-1 w-full">
+              <span className="text-white ">{user.name}</span>
+            </div>
+          </div>)
+        })
+      }
+     
     </div>
+
   )
 }
